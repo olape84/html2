e56f59c00c500405b46607f575e6d795ca21ad3d
Aleksandra Przywrzej <przepjurka@gmail.com>
2020-09-24 17:37:11 +0200

    zmienione linki wew plikow ale nadal nie dzialaja

--- card.py
+++ card.py
@@ -6,6 +6,15 @@ app=Flask(__name__)
 def home():
     return render_template("index.html")
 
+@app.route('/message', methods=['GET', 'POST'])
+def message():
+   if request.method == 'GET':
+       print("We received GET")
+       return render_template("form.html")
+   elif request.method == 'POST':
+       print("We received POST")
+       print(request.form)
+       return redirect("/")
 
 if __name__ == "__main__":
     app.run()
